a66380e85611a36c52b0e13242504f7ce2a6a828
ivan.belyaev <belivan84@gmail.com>
2023-11-01 00:06:24 +0300

    проверка работы балансировщика нагрузки

--- eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/test")
     public String test() {
-        return "new test string";
+        return instanceId;
     }
 }
